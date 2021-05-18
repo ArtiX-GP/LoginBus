1eb0da2574d3a16786d2e1e44f001e5424068b84
ArtemKar123 <wartemtem@yandex.ru>
2021-05-18 10:46:02 +0800

    Scene managment

--- Assets/Source/Character/CameraFollow2D.cs
+++ Assets/Source/Character/CameraFollow2D.cs
@@ -17,7 +17,7 @@ public class CameraFollow2D : MonoBehaviour {
 
 	public void FindPlayer(bool playerFaceLeft)
 	{
-		player = GameObject.FindGameObjectWithTag("Player").transform;
+		player = firstPlayer; //GameObject.FindGameObjectWithTag("Player").transform;
 		lastX = Mathf.RoundToInt(player.position.x);
 		if (playerFaceLeft) {
 			transform.position = new Vector3(player.position.x - offset.x, player.position.y + offset.y, transform.position.z);
